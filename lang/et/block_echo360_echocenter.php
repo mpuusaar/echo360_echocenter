bb704a1b38e50c7b82f7232e79bf4ae0576c170d
Marko Puusaar <marko@estmarx.pri.ee>
2013-07-23 18:44:26 +0300

    * " => ' - to avoid misinterpreting variables inside language strings * Added Estonian translation

---
+++ lang/et/block_echo360_echocenter.php
@@ -0,0 +1,40 @@
+<?php
+
+// This file is part of the Echo360 Moodle Plugin - http://moodle.org/
+//
+// The Echo360 Moodle Plugin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Echo360 Moodle Plugin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the Echo360 Moodle Plugin.  If not, see <http://www.gnu.org/licenses/>.
+ 
+/**
+ * This file provides the language strings for the echo360_echocenter block
+ *
+ * @package    block
+ * @subpackage echo360_echocenter
+ * @copyright  2011 Echo360 Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later 
+ */
+
+    $string['echocenter'] = 'Echo360 EchoCenter';
+    $string['echocenter_link'] = 'Ava EchoCenter...';
+    $string['loading'] = 'Laen. Palun oota...';
+
+    $string['pluginname'] = 'Echo360 EchoCenter';
+    $string['openinnewwindow'] = 'Ava uues aknas...';
+    $string['not_configured'] = 'See plokk vajab süsteemi tasemel seadistamistamist enne kui seda saab kasutada.';
+    $string['configure_link_text'] = 'Klõpsa siia, et teostada süsteemi tasemel seadistused selle ploki jaoks.';
+    $string['error_generating_url'] = 'Selle ressursi jaoks URLi loomisel juhtus viga. Palun võta ühendust süsteemiadministraatoriga. <br/> {$a}';
+    $string['no_view_capability'] = 'Sul puuduvad õigused selle sisu vaatamiseks.';
+    $string['not_found_response'] = 'EchoSystem ei suutnud leida vastavat kursust. Võta ühendust administraatoriga, et veenduda loengusalvestuste loomises käesoleva kursuse jaoks. <br/> {$a}';
+    $string['forbidden_response'] = 'EchoSystem ei luba ligipääsu. Võta ühendust administraatoriga, et veenduda käesoleva Moodle mooduli korrektses seadistuses. <br/> {$a->classname}';
+    $string['unexpected_response'] = 'EchoSystem andis ootamatu vastuse. Võta ühendust administraatoriga, et veenduda käesoleva Moodle mooduli korrektses seadistuses. <br/> {$a}';
+?>
